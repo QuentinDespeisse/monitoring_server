9e0f55dd0891a5ca4c4d6d569b2e765cb325e83d
gladeroz <julien.barreault@atos.net>
2018-09-08 15:37:46 +0200

    JBR : rename project

--- content/server/PROD.php
+++ content/server/PROD.php
@@ -1,72 +1,79 @@
 <?php
-	include $_SERVER['DOCUMENT_ROOT']."/enedis/include/header.inc.php"
+include $_SERVER['DOCUMENT_ROOT']."/enedis/include/header.inc.php"
 ?>          
 
-		  <div class="content mt-3">
-            <div class="animated fadeIn">
-   
-   <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <strong class="card-title">Informations générales servers</strong>
-                        </div>
-                        <div class="card-body">
-                            <table class="table table-striped">
-                              <thead>
-                                <tr>
-                                  <th scope="col">#</th>
-                                  <th scope="col">Apache frontal</th>
-                                  <th scope="col">Instance</th>
-                                  <th scope="col">User</th>
-								  <th scope="col">Password</th>
-								  <th scope="col">Etat</th>
-								  <th scope="col"></th>
-								  <th scope="col"></th>
-								  <th scope="col"></th>
-                                </tr>
-                              </thead>
-                              <tbody>
-                                <tr>
-                                  <th scope="row">1</th>
-                                  <td><a href="http://apache.enedis.fr">http://apache.enedis.fr</a></td>
-                                  <td><a href="http://instance1.apache.enedis.fr">http://instance1.apache.enedis.fr</a></td>
-                                  <td>@mdo</td>
-								  <td>@mdo1</td>
-								  <td><span class="ti-heart" style="color:green"/></td>
-								  <td><button type="button" class="btn btn-outline-success"><i class="fa fa-rss"></i>&nbsp; Start</button></td>
-								  <td><button type="button" class="btn btn-outline-danger"><i class="fa fa-rss"></i>&nbsp; Stop</button></td>
-								  <td><button type="button" class="btn btn-outline-link"><i class="fa fa-link"></i>&nbsp; Restart</button></td>
-                                </tr>
-                                <tr>
-                                  <th scope="row">2</th>
-                                <td><a href="http://apache.enedis.fr">http://apache.enedis.fr</a></td>
-                                  <td><a href="http://instance2.apache.enedis.fr">http://instance2.apache.enedis.fr</a></td>
-                                  <td>@mdo</td>
-								  <td>@mdo2</td>
-								  <td><span class="ti-heart-broken" style="color:red"/></td>
-								  <td><button type="button" class="btn btn-outline-success"><i class="fa fa-rss"></i>&nbsp; Start</button></td>
-								  <td><button type="button" class="btn btn-outline-danger"><i class="fa fa-rss"></i>&nbsp; Stop</button></td>
-								  <td><button type="button" class="btn btn-outline-link"><i class="fa fa-link"></i>&nbsp; Restart</button></td>
-                                </tr>
-                                <tr>
-                                  <th scope="row">3</th>
-                                  <td><a href="http://apache.enedis.fr">http://apache.enedis.fr</a></td>
-                                  <td><a href="http://instance3.apache.enedis.fr">http://instance3.apache.enedis.fr</a></td>
-                                  <td>@mdo</td>
-								  <td>@mdo3</td>
-								  <td><span class="ti-heart" style="color:green"/></td>
-								  <td><button type="button" class="btn btn-outline-success"><i class="fa fa-rss"></i>&nbsp; Start</button></td>
-								  <td><button type="button" class="btn btn-outline-danger"><i class="fa fa-rss"></i>&nbsp; Stop</button></td>
-								  <td><button type="button" class="btn btn-outline-link"><i class="fa fa-link"></i>&nbsp; Restart</button></td>
-                                </tr>
-                              </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-				</div>
-				</div>
-				
+<div class="breadcrumbs">
+  <div class="col-sm-4">
+    <div class="page-header float-left">
+      <div class="page-title">
+        <h1>Plateforme de production</h1>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-8">
+    <div class="page-header float-right">
+      <div class="page-title">
+        <ol class="breadcrumb text-right">
+          <li class="active">Plateforme de production</li>
+        </ol>
+      </div>
+    </div>
+  </div>
+</div>
+
 <?php
-	include $_SERVER['DOCUMENT_ROOT']."/enedis/include/footer.inc.php"
+include $_SERVER['DOCUMENT_ROOT']."/enedis/include/topbar.inc.php"
+?> 
+
+<div class="content mt-3">
+  <div class="animated fadeIn">
+
+   <div class="col-lg-6">
+    <div class="card">
+      <div class="card-header">
+        <strong class="card-title">Informations générales servers</strong>
+      </div>
+      <div class="card-body">
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Apache frontal</th>
+              <th scope="col">Instance</th>
+              <th scope="col">User</th>
+              <th scope="col">Password</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td><a>http://apache.enedis.fr</a></td>
+              <td><a>http://instance1.apache.enedis.fr</a></td>
+              <td>@mdo</td>
+              <td>@mdo1</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td><a>http://apache.enedis.fr</a></td>
+              <td><a>http://instance2.apache.enedis.fr</a></td>
+              <td>@mdo</td>
+              <td>@mdo2</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td><a>http://apache.enedis.fr</a></td>
+              <td><a>http://instance3.apache.enedis.fr</a></td>
+              <td>@mdo</td>
+              <td>@mdo3</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<?php
+include $_SERVER['DOCUMENT_ROOT']."/enedis/include/footer.inc.php"
 ?>
